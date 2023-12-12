814f0510f4a98f7cf09f2f47b60756a1a2e558a3
SicerAndresBritoGutierrezCAMPUS <126881720+SicerBrito@users.noreply.github.com>
2023-12-12 13:30:49 -0500

    Create ahorcado.py

---
+++ ahorcado.py
@@ -0,0 +1,28 @@
+def jugar_ahorcado():
+    palabra_secreta = "python"  # Puedes cambiar la palabra según tu preferencia
+    letras_adivinadas = []
+    intentos_fallidos_maximos = 3
+
+    print("¡Bienvenido al juego del ahorcado!")
+
+    while intentos_fallidos_maximos > 0:
+        palabra_mostrada = ''.join(letra if letra in letras_adivinadas else '_' for letra in palabra_secreta)
+        print("Palabra a adivinar: " + palabra_mostrada)
+
+        if palabra_mostrada == palabra_secreta:
+            print("¡Felicidades! Has adivinado la palabra.")
+            break
+
+        letra = input("Ingresa una letra: ").lower()
+
+        if letra not in palabra_secreta:
+            intentos_fallidos_maximos -= 1
+            print("Incorrecto. Intentos fallidos restantes:", intentos_fallidos_maximos)
+        
+        letras_adivinadas.append(letra)
+
+    if intentos_fallidos_maximos == 0:
+        print("¡Oh no! Has agotado tus intentos fallidos. La palabra era:", palabra_secreta)
+
+if __name__ == "__main__":
+    jugar_ahorcado()
